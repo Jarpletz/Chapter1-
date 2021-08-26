ea1251483782bee617370f14b28b623e89b82603
Jarpletz <89551632+Jarpletz@users.noreply.github.com>
2021-08-26 16:02:31 -0400

    Add files via upload

---
+++ First C++ Program!/First C++ Program!/First C++ Program!.cpp
@@ -0,0 +1,15 @@
+// First C++ Program!.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Roses Are Red"<<endl;
+    cout << "Violets Are Blue" << endl;
+    cout << "This technically is coding" << endl;
+    cout << "and poetry too!\n\n";
+    return 0;
+}
+
