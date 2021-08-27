e61bd16c4c7c75129be5c4915186271623381898
Jarpletz <rothpletz5@gmail.com>
2021-08-27 14:20:58 -0400

    x

---
+++ week1HW/OutputBinaryDefs.cpp
@@ -0,0 +1,16 @@
+// week1HW.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout << "Bit: A single bianary value, the base of all computing information and storage."<<endl;
+   cout << "Byte: stores 8 bits, 2^8=256 possible values per bit; used to store number and letter values, as well as basic functions." << endl;
+   cout << "ex. '3' is stored as '00110011.'" << endl;
+   cout << "1 Kilobyte= 2^10 bytes" << endl;
+   cout << "1 Megabyte= 2^20 bytes= 2^10 KB" << endl;
+   cout << "1 Gigabyte= 2^30 bytes= 2^10 MB" << endl;
+   cout << "1 Terabyte= 2^40 bytes= 2^10 GB" << endl;  
+   return 0;
+}
