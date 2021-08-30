57a6d1ec08607680ee89939bba86aeddc96c79fe
Jarpletz <rothpletz5@gmail.com>
2021-08-30 15:37:14 -0400

    c

--- week1HW/OutputBinaryDefs.cpp
+++ week1HW/OutputBinaryDefs.cpp
@@ -7,7 +7,7 @@ int main()
 {
    cout << "Bit: A single bianary value, the base of all computing information and storage."<<endl;
    cout << "Byte: stores 8 bits, 2^8=256 possible values per bit; used to store number and letter values, as well as basic functions." << endl;
-   cout << "ex. '3' is stored as '00110011.'" << endl;
+   cout << "ex. '3' is stored as '00110011' in ASCII." << endl;
    cout << "1 Kilobyte= 2^10 bytes" << endl;
    cout << "1 Megabyte= 2^20 bytes= 2^10 KB" << endl;
    cout << "1 Gigabyte= 2^30 bytes= 2^10 MB" << endl;
